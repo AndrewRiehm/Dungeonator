885c6617a31c74641f2252df65260155d0c74836
Andrew Riehm <andrew.riehm@gmail.com>
2016-01-24 12:26:35 -0700

    campaignsCtrl now uses NewWidget

--- app/www/components/campaigns/campaignsCtrl.js
+++ app/www/components/campaigns/campaignsCtrl.js
@@ -7,43 +7,24 @@
         .module('starter.controllers')
         .controller('CampaignsCtrl', campaignsCtrlFunc);
 
-    function campaignsCtrlFunc($scope, Campaigns, $ionicPopup) {
+    function campaignsCtrlFunc($scope, NewWidget, Campaigns, $ionicPopup) {
         $scope.campaigns = Campaigns.all();
         $scope.remove = function(campaign) {
             Campaigns.remove(campaign);
         };
 
-        // $scope.showNewForm = showNewForm.bind(null, $scope, $ionicPopup, Campaigns);
-    }
-
-    function showNewForm($scope, $ionicPopup, Campaigns) {
-        $ionicPopup.prompt(
-        {
-            title: 'New Campaign',
-            inputType: 'text',
-            inputPlaceholder: 'Campaign Name'
-        })
-        .then(function(res) {
-            if (res !== undefined) {
-                addNewCampaign(Campaigns, res, $ionicPopup);
-            }
-        });
-    }
-
-    function addNewCampaign(Campaigns, campaignName, $ionicPopup) {
-
-        var obj = {
-            name: campaignName
-        };
-
-        Campaigns.add(obj, function(err) {
-            if (err) {
-                $ionicPopup.alert({
-                    title: 'Oops!',
-                    template: '"' + campaignName + '" is already taken!',
+        $scope.showNewForm = function() {
+            NewWidget.show('New Campaign', function(name) {
+                var obj = { name: name };
+                Campaigns.add(obj, function(err) {
+                    if (err) {
+                        $ionicPopup.alert({
+                            title: 'Oops!',
+                            template: '"' + name + '" is already taken!',
+                        });
+                    }
                 });
-            }
-        });
+            });
+        };
     }
-
 })();
