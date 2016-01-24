87fdb2676ea2789e9782548503004c1c13afedc5
Andrew Riehm <andrew.riehm@gmail.com>
2016-01-24 09:17:04 -0700

    added functionality to campaignsCtrl, and tests

--- app/www/components/campaigns/campaignsCtrl.js
+++ app/www/components/campaigns/campaignsCtrl.js
@@ -7,8 +7,43 @@
         .module('starter.controllers')
         .controller('CampaignsCtrl', campaignsCtrlFunc);
 
-    function campaignsCtrlFunc($scope) {
-        $scope.foo = 'bar';
+    function campaignsCtrlFunc($scope, Campaigns, $ionicPopup) {
+        $scope.campaigns = Campaigns.all();
+        $scope.remove = function(campaign) {
+            Campaigns.remove(campaign);
+        };
+
+        // $scope.showNewForm = showNewForm.bind(null, $scope, $ionicPopup, Campaigns);
+    }
+
+    function showNewForm($scope, $ionicPopup, Campaigns) {
+        $ionicPopup.prompt(
+        {
+            title: 'New Campaign',
+            inputType: 'text',
+            inputPlaceholder: 'Campaign Name'
+        })
+        .then(function(res) {
+            if (res !== undefined) {
+                addNewCampaign(Campaigns, res, $ionicPopup);
+            }
+        });
+    }
+
+    function addNewCampaign(Campaigns, campaignName, $ionicPopup) {
+
+        var obj = {
+            name: campaignName
+        };
+
+        Campaigns.add(obj, function(err) {
+            if (err) {
+                $ionicPopup.alert({
+                    title: 'Oops!',
+                    template: '"' + campaignName + '" is already taken!',
+                });
+            }
+        });
     }
 
 })();
