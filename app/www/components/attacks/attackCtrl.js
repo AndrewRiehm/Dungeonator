53de0e424d090a809de60712bddbe682d5cff214
Andrew Riehm <andrew.riehm@gmail.com>
2016-01-24 17:54:50 -0700

    minor lint cleanup

--- app/www/components/attacks/attackCtrl.js
+++ app/www/components/attacks/attackCtrl.js
@@ -6,20 +6,23 @@
         .module('starter.controllers')
         .controller('AttackCtrl', ctrlFunc);
 
-    function ctrlFunc($scope,$stateParams, Characters, $ionicPopup) {
+    function ctrlFunc($scope,$stateParams, Characters) {
+        var msg;
+
         $scope.characterName = $stateParams.characterName;
         $scope.character = Characters.get($scope.characterName);
         $scope.weaponName = $stateParams.weaponName;
         $scope.weapon = Characters.getWeapon($scope.character, $scope.weaponName);
         $scope.attackName = $stateParams.attackName;
         $scope.attack = getAttack($scope.weapon, $scope.attackName);
+
         if ($scope.weapon === null) {
-            var msg = 'ERROR: could not get weapon with name "' + $scope.weaponName + '"';
+            msg = 'ERROR: could not get weapon with name "' + $scope.weaponName + '"';
             console.error(msg);
             throw(msg);
         }
         if ($scope.attack === null) {
-            var msg = 'ERROR: could not get attack with name "' + $scope.attackName + '"';
+            msg = 'ERROR: could not get attack with name "' + $scope.attackName + '"';
             console.error(msg);
             throw(msg);
         }
