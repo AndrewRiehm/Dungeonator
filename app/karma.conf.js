0a912a3fe9517601ec479f4a7df4aec405f984f3
Andrew Riehm <andrew.riehm@gmail.com>
2016-01-25 18:06:42 -0700

    added angular-webstorage library through bower

--- app/karma.conf.js
+++ app/karma.conf.js
@@ -16,6 +16,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       'www/lib/ionic/js/ionic.bundle.js',
+      'www/lib/angular-webstorage/angular-webstorage.js',
       'www/lib/angular-mocks/angular-mocks.js',
       'www/app.js',
       'www/components/**/*.js'
